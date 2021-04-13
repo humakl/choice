042ed515b67da8846a618dbf2ecd153788b9b589
humakl <st.sinnerman@gmail.com>
2021-04-13 14:45:39 +0400

    added initialization;

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Nav from './components/nav/nav';
-import { Route } from 'react-router-dom';
+import { Route, withRouter } from 'react-router-dom';
 import News from './components/news/news';
 import Photos from './components/photos/photos';
 import Settings from './components/settings/settings';
@@ -9,28 +9,49 @@ import UsersContainer from './components/users/usersContainer';
 import ProfileContainer from './components/profile/profileContainer';
 import HeaderContainer from './components/header/headerContainer';
 import LoginPage from './components/login/login';
+import { Component } from 'react';
+import { connect } from 'react-redux';
+import { initializeApp } from "./redux/appReducer";
+import { compose } from 'redux';
+import Preloader from './components/common/preloader/preloader';
 
 
-const App = () => {
-  return (
-    <div className="app-wrapper">
-      <HeaderContainer />
-      <Nav />
-      <div className="app-content-wrapper">
-        <Route path="/profile/:userId?"
-          render={() => <ProfileContainer />} />
-        <Route path="/dialogs"
-          render={() => <DialogsContainer />} />
-        <Route path="/users"
-          render={() => <UsersContainer />} />
-        <Route path="/login"
-          render={() => <LoginPage />} />
-        <Route path="/news" render={() => <News />} />
-        <Route path="/photos" render={() => <Photos />} />
-        <Route path="/settings" render={() => <Settings />} />
+class App extends Component {
+  componentDidMount() {
+    this.props.initializeApp();
+  }
+  render() {
+
+    if (!this.props.initialized) {
+      return <Preloader />
+    }
+
+    return (
+      <div className="app-wrapper">
+        <HeaderContainer />
+        <Nav />
+        <div className="app-content-wrapper">
+          <Route path="/profile/:userId?"
+            render={() => <ProfileContainer />} />
+          <Route path="/dialogs"
+            render={() => <DialogsContainer />} />
+          <Route path="/users"
+            render={() => <UsersContainer />} />
+          <Route path="/login"
+            render={() => <LoginPage />} />
+          <Route path="/news" render={() => <News />} />
+          <Route path="/photos" render={() => <Photos />} />
+          <Route path="/settings" render={() => <Settings />} />
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  initialized: state.app.initialized
+})
+
+export default compose(
+  withRouter,
+  connect(mapStateToProps, { initializeApp }))(App);
---
+++ src/redux/appReducer.js
@@ -0,0 +1,33 @@
+import { getAuthUserData } from "./authReducer";
+
+const INITIALIZATION_SUCCESS = 'INITIALIZATION_SUCCESS';
+
+let initialState = {
+  initialized: false
+};
+
+const appReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case INITIALIZATION_SUCCESS:
+      return {
+        ...state,
+        initialized: true
+      }
+
+    default:
+      return state;
+  }
+}
+
+export const initializationSuccess = () => ({ type: INITIALIZATION_SUCCESS });
+
+export const initializeApp = () => (dispatch) => {
+  let promise = dispatch(getAuthUserData());
+  Promise.all([promise])
+    .then(() => {
+      dispatch(initializationSuccess());
+    })
+
+}
+
+export default appReducer;
