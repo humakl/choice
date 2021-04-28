30981ab44a3a028ceba5a857941d43befb1faeaf
humakl <st.sinnerman@gmail.com>
2021-04-28 20:36:10 +0400

    refactoring in usersContainer

--- src/components/users/UsersContainer.jsx
+++ src/components/users/UsersContainer.jsx
@@ -10,12 +10,13 @@ import { getPageSize, getAllUsers, getTotalUSersCount, getCurrentPage, getIsFetc
 class UsersContainer extends React.Component {
 
   componentDidMount() {
-    this.props.getUsers(this.props.currentPage, this.props.pageSize);
+    const {currentPage, pageSize} = this.props;
+    this.props.getUsers(currentPage, pageSize);
   }
 
   onPageChanged = (pageNumber => {
-
-    this.props.getUsers(pageNumber, this.props.pageSize);
+    const {pageSize} = this.props;
+    this.props.getUsers(pageNumber, pageSize);
   })
 
   render() {
@@ -34,18 +35,6 @@ class UsersContainer extends React.Component {
     </>
   }
 }
-
-// let mapStateToProps = (state) => {
-//   //возвращает объект, который по итогу из state достает данные
-//   return {
-//     users: state.usersPage.users,
-//     pageSize: state.usersPage.pageSize,
-//     totalUsersCount: state.usersPage.totalUsersCount,
-//     currentPage: state.usersPage.currentPage,
-//     isFetching: state.usersPage.isFetching,
-//     followingInProgress: state.usersPage.followingInProgress
-//   }
-// }
 
 let mapStateToProps = (state) => {
   return {
